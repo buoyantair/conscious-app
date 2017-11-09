32c1791a08682b9db8ee929d912462b926141405
buoyantair <buoyantair@gmail.com>
2017-11-09 10:53:07 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import '../node_modules/normalize.css/normalize.css';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        App component
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
